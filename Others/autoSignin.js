8fb42e031f5098ba6cc751f6bb3c5c377f0e8947
Jhon-Tobey <xqwkuaile@gmail.com>
2023-03-23 10:12:22 +0800

    调整

--- Others/autoSignin.js
+++ Others/autoSignin.js
@@ -3,8 +3,8 @@ cron "0 9 * * *" autoSignin.js, tag=阿里云盘签到
 */
 
 const axios = require('axios')
-const { initInstance, getEnv, updateCkEnv } = require('./tools/qlApi.js')
-const notify = require('./tools/sendNotify')
+const { initInstance, getEnv, updateCkEnv } = require('./qlApi.js')
+const notify = require('./sendNotify')
 
 const updateAccesssTokenURL = 'https://auth.aliyundrive.com/v2/account/token'
 const signinURL = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
